cde521d183c37a5b6af5698dd4eead06fce48701
Gio <gio@example.com>
2018-11-11 16:24:09 -0500

    Updated getting direction going home to take wrapping into account

--- src/common/movement.py
+++ src/common/movement.py
@@ -4,6 +4,7 @@ from hlt.game_map import GameMap
 from hlt.positionals import Position
 from hlt.positionals import Direction
 import abc
+import itertools
 
 class Moves(abc.ABC):
     """
@@ -53,7 +54,9 @@ class Moves(abc.ABC):
         :return: DIRECTION TO SHIPYARD POSITION
         """
 
-        choices = GameMap._get_target_direction(ship_position, home_position)   ## WILL GIVE LONGER PATH, IF WRAPPING
+        #choices = GameMap._get_target_direction(ship_position, home_position)   ## WILL GIVE LONGER PATH, IF WRAPPING
+        choices = self.get_target_direction(ship_position, home_position, self.data.map_width)  ## NOT WORKING RIGHT YET
+
         clean_choices = [x for x in choices if x != None]                       ## CAN HAVE A NONE
         logging.debug("ship position: {} shipyard position: {} clean_choices: {}".format(ship_position,
                                                                                          home_position,
@@ -63,6 +66,60 @@ class Moves(abc.ABC):
         logging.debug("chosen direction: {}".format(direction))
 
         return direction
+
+
+    def get_target_direction(self, start, destination, size):
+        """
+        GET DIRECTION TOWARDS TARGET, TAKE WRAPPING INTO ACCOUNT
+
+        :param start: START POSITION
+        :param destination: DESTINATION POSITION
+        :param size: MATRIX SIZE
+        :return: LIST OF POSSIBLE DIRECTIONS TOWARD TARGET
+        """
+
+        def get_mirror_locations(start, destination, size):
+            """
+            GET MIRROR LOCATIONS, SHOULD BE 4 ALWAYS WITH WRAPPING
+
+            :return: TUPLE OF LOCATIONS
+            """
+            if start.y > destination.y:
+                ys = [destination.y, destination.y + size]
+            else:
+                ys = [destination.y, destination.y - size]
+
+            if start.x > destination.x:
+                xs = [destination.x, destination.x + size]
+            else:
+                xs = [destination.x, destination.x - size]
+
+            return itertools.product(ys, xs)
+
+        def get_closest_location(start, locations):
+            """
+            GET CLOSEST LOCATION
+            BASED ON ALL MIRROR LOCATIONS
+
+            :return: TUPLE OF BEST LOCATION
+            """
+            closest = 9999999
+            closest_location = None
+
+            for dest in locations:
+                dist = abs(dest[0] - start.y) + abs(dest[1] - start.x)
+                if dist < closest:
+                    closest = dist
+                    closest_location = dest
+
+            return closest_location
+
+
+        all_locations = get_mirror_locations(start, destination, size)
+        best_location = get_closest_location(start, all_locations)
+
+        x, y = best_location[1], best_location[0]
+        return GameMap._get_target_direction(start, Position(x, y))
 
 
     def get_destination(self, ship, direction):
