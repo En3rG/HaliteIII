2117fbc1dab3cc77f9e3f134cc78d6590f5f1cf5
Gio <gio@example.com>
2018-12-07 11:38:20 -0500

    Updated min number of ships to build to 14 and extra turn retreat to 3

--- src/common/matrix/data.py
+++ src/common/matrix/data.py
@@ -439,12 +439,20 @@ class Data(abc.ABC):
         POPULATE TOP HALITE CELLS
         LIMIT TO LOCAL AREA WITHIN THE FIRST 100 MOVES
         """
+        ## ORIGINAL
         top_num_cells = int(MyConstants.TOP_N_HALITE * (self.game.game_map.height * self.game.game_map.width))
         top, ind = get_n_largest_values(self.myMatrix.halite.amount, top_num_cells)
         self.myMatrix.halite.top_amount[ind] = Matrix_val.TEN
 
+        ## USED WHEN THE TOP HALITE PERCENTAGE IS LOW (< 2%)
+        # top_num_cells = int(MyConstants.TOP_N_HALITE * (self.game.game_map.height * self.game.game_map.width))
+        # if top_num_cells < len(self.mySets.ships_all):
+        #     top_num_cells = len(self.mySets.ships_all)
+        # top, ind = get_n_largest_values(self.myMatrix.halite.amount, top_num_cells)
+        # self.myMatrix.halite.top_amount[ind] = Matrix_val.TEN
+
         ## LIMITING EARLY GAME TO STAY CLOSE BY
-        ## SEEMS BETTER TO LIMIT BUILDING BASED NUMBER OF SHIPS 
+        ## SEEMS BETTER TO LIMIT BUILDING BASED NUMBER OF SHIPS
         # if self.game.turn_number < MyConstants.EARLY_GAME_TURNS:
         #     mask = np.zeros((self.game.game_map.height, self.game.game_map.width), dtype=np.int16)
         #     populate_manhattan(mask, 1, self.game.me.shipyard.position, MyConstants.MIN_DIST_BTW_DOCKS, cummulative=False)
