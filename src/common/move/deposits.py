6ca62f4a4b506ee711472eae946fb618a21d7b5c
Gio <gio@example.com>
2019-01-19 00:00:45 -0500

    Update depositNow, when it can harvest, to check if it has potential enemy collision, if so dont harvest

--- src/common/move/deposits.py
+++ src/common/move/deposits.py
@@ -21,7 +21,10 @@ class Deposits(abc.ABC):
 
         if harvest:
             current_harvest = self.data.myMatrix.halite.harvest_with_bonus[ship.position.y][ship.position.x]
-            if current_harvest >= self.data.myVars.deposit_harvest_percentile and ship.halite_amount + current_harvest < 1000:
+            potential_enemy_collision = self.data.myMatrix.locations.potential_enemy_collisions[ship.position.y][ship.position.x]
+            if current_harvest >= self.data.myVars.deposit_harvest_percentile \
+                    and ship.halite_amount + current_harvest < 1000 \
+                    and potential_enemy_collision == Matrix_val.ZERO:
                 direction = Direction.Still
 
         self.move_mark_unsafe(ship, direction)
